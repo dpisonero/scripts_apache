55409d5e1e333de66e9b421a7bde705b3c0bdaa1
dpisonero <davidpisonero96@gmail.com>
2018-12-22 18:10:27 +0100

    Install bien

--- InstallApache.py
+++ InstallApache.py
@@ -8,15 +8,15 @@ subprocess.call(["apt-get", "install", "curl"])
 
 subprocess.call(["sudo", "cp", "/etc/apache2/sites-available/000-default.conf", "/etc/apache2/sites-available/dominio1.conf"])
 
-#fin = open("/etc/apache2/sites-available/000-default.conf", 'r') # in file
+fin = open("/etc/apache2/sites-available/000-default.conf", 'r') # in file
 fout = open("/etc/apache2/sites-available/dominio1.conf ", 'r+') # out file
-for line in fout:
+for line in fin:
 	if "DocumentRoot /var/www/html" in line :
 		fout.write("\n ServerName dominio1.cdps \n ServerAlias www.dominio1.cdps \n")
 	else:
 		fout.write(line)
 
-#fin.close()
+fin.close()
 fout.close()
 subprocess.call(["sudo", "a2ensite", "dominio1.conf"])
 subprocess.call(["service" , "apache2", "reload"])
